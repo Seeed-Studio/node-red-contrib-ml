8c62e75dad219ebc12fcb1b153ee25106afba994
Baozhu Zuo <zuobaozhu@gmail.com>
2022-05-06 16:58:50 +0800

    use http and zmq to broadcast camera data

--- python/pub_sub_broadcast.py
+++ python/pub_sub_broadcast.py
@@ -16,19 +16,43 @@ import os
 import argparse
 import base64
 
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+from multiprocessing import Process
+import subprocess
+import threading
+from flask import Flask
+
+import logging
+logging.getLogger('werkzeug').disabled = True
+
+app = Flask(__name__)
+
+host = ('0.0.0.0', 5550)
+
+to_jpg_buffer = bytes('seeed',encoding="utf-8")
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='broadcast OpenCV stream using PUB SUB.')
-    parser.add_argument('--port', type=int, required=True, help='video stream broadcast port')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
-    args = parser.parse_args()
+    return 5555, '0'
 
-    return args.port, args.source
+# def parse_args():
+#     parser = argparse.ArgumentParser(description='broadcast OpenCV stream using PUB SUB.')
+#     parser.add_argument('--port', type=int, required=True, help='video stream broadcast port')
+#     parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
+#     args = parser.parse_args()
 
+#     return args.port, args.source
 
+@app.route('/health')
+def http_get_request_health():
+    return 'ok'
 
-if __name__ == "__main__":
+@app.route('/')
+def hello():
+    return base64.b64encode(to_jpg_buffer)
+
+def start_streams():
+    global to_jpg_buffer
     port, source = parse_args()
     sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
 
@@ -51,14 +75,22 @@ if __name__ == "__main__":
         counter = 0
         for path, im, im0s, vid_cap, s in dataloader:
             for img in im0s:
-                #img = cv2.resize(img, (320,240), interpolation=cv2.INTER_LINEAR)
+                # img = cv2.resize(img, (640,360), interpolation=cv2.INTER_LINEAR)
                 ret_code, jpg_buffer = cv2.imencode(
                     ".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
                 # sender.send_jpg(rpi_name, base64.b64encode(jpg_buffer))
+
+                to_jpg_buffer = jpg_buffer
+
                 sender.send_jpg(rpi_name, jpg_buffer)
+
                 #sender.send_jpg(rpi_name, img)
-                #print("Sent frame {}".format(counter))
-                #sleep(0.05)
+
+                # jancee updated
+                # print("Sent frame {}".format(counter))
+
+                # Jancee updated
+                # sleep(0.05)
                 counter = counter + 1
 
     except (KeyboardInterrupt, SystemExit):
@@ -70,3 +102,11 @@ if __name__ == "__main__":
     finally:
         sender.close()
         sys.exit()
+
+def init_streams():
+    t = threading.Thread(target=start_streams)
+    t.start()
+
+if __name__ == "__main__":
+    init_streams()
+    app.run(host="0.0.0.0", port=5550)
