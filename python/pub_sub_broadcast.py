aa358a4c2e4ea6e58f98900bd8c304704536ac3f
Baozhu Zuo <zuobaozhu@gmail.com>
2022-05-13 09:41:41 +0800

    rewrite dataloader sevice

--- python/pub_sub_broadcast.py
+++ python/pub_sub_broadcast.py
@@ -22,18 +22,21 @@ from multiprocessing import Process
 import subprocess
 import threading
 from flask import Flask
+from flask import request
+import numpy as np
+import re
 
 import logging
-logging.getLogger('werkzeug').disabled = True
+# logging.getLogger('dataloader').disabled = True
 
 app = Flask(__name__)
 
 host = ('0.0.0.0', 5550)
 
-to_jpg_buffer = bytes('seeed',encoding="utf-8")
-
 def parse_args():
     return 5555, '0'
+
+
 
 # def parse_args():
 #     parser = argparse.ArgumentParser(description='broadcast OpenCV stream using PUB SUB.')
@@ -43,70 +46,135 @@ def parse_args():
 
 #     return args.port, args.source
 
-@app.route('/health')
-def http_get_request_health():
-    return 'ok'
+def generate_image_with_text(text):
+    img = np.ones(shape=(480,640,3), dtype=np.int16)
+    y0, dy = 30, 30
+    #auto line , 35 ch new line
+    for i, line in enumerate(re.findall(r'.{35}', text)):
+        y = y0 + i*dy
+        # cv2.putText(img, line, (0, y ), cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
+        cv2.putText(img=img, text=line, org=(0, y), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 255, 0),thickness=1)
 
-@app.route('/')
-def hello():
-    return base64.b64encode(to_jpg_buffer)
+    # cv2.putText(img=img, text=text, org=(0, 30), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 255, 0),thickness=1)
+    ret_code, jpg_buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY)])
+    return jpg_buffer
 
-def start_streams():
-    global to_jpg_buffer
-    port, source = parse_args()
-    # sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
 
-    # Open input stream; comment out one of these capture = VideoStream() lines!
-    # *** You must use only one of Webcam OR PiCamera
-    # Webcam source for broadcast images
-    dataloader = LoadStreams(source)  # Webcam
 
-    # JPEG quality, 0 - 100
-    jpeg_quality = 95
 
-    # Send RPi hostname with each image
-    # This might be unnecessary in this pub sub mode, as the receiver will
-    #    already need to know our address and can therefore distinguish streams
-    # Keeping it anyway in case you wanna send a meaningful tag or something
-    #    (or have a many to many setup)
-    rpi_name = socket.gethostname()
 
-    try:
-        counter = 0
-        for path, im, im0s, vid_cap, s in dataloader:
-            for img in im0s:
-                img = cv2.resize(img, (1280,720), interpolation=cv2.INTER_LINEAR)
-                ret_code, jpg_buffer = cv2.imencode(
-                    ".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-                # sender.send_jpg(rpi_name, base64.b64encode(jpg_buffer))
+class DataLoadderThread(threading.Thread):
+    # overriding constructor
+    def __init__(self):
+        # calling parent class constructor
+        threading.Thread.__init__(self)
+        # JPEG quality, 0 - 100
+        self.jpeg_quality = 95
+        self.data = None
+        self.is_init = False
+        self.is_ok = True
+        self.screen_table = {'3840': {'h':3840,'w':2160}, 
+                '2560': {'h':2560,'w':1600}, 
+                '1920': {'h':1920,'w':1080}, 
+                '1280': {'h':1280,'w':720}, 
+                '800': {'h':800,'w':600}, 
+                '640': {'h':640,'w':480}, 
+                '320': {'h':320,'w':320}}
 
-                to_jpg_buffer = jpg_buffer
 
-                # sender.send_jpg(rpi_name, jpg_buffer)
+    # define your own run method
+    def run(self):
+        self.start_streams()
+    def set_args(self,resolution,device,rtsp):
+        self.resolution = resolution
+        self.device = device
+        self.rtsp = rtsp
 
-                #sender.send_jpg(rpi_name, img)
+    def start_streams(self):
+        port, source = parse_args()
+        # sender = imagezmq.ImageSender("tcp://*:{}".format(port), REQ_REP=False)
 
-                # jancee updated
-                # print("Sent frame {}".format(counter))
+        if not self.is_init:
+            self.data = generate_image_with_text("not init....")
+            while not self.is_init:
+                try:
+                    if  self.rtsp :
+                        source = ('rtspsrc location={} ! ''rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv !'
+               'video/x-raw,width={},height={},format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink ').format(self.rtsp,
+                                                                                                                self.screen_table[self.resolution]['w'],
+                                                                                                                self.screen_table[self.resolution]['h'])
+                    else:
+                        source ("v4l2src device={} ! image/jpeg,framerate=30/1,width={}, height={},type=video ! "
+                        "jpegdec ! videoconvert ! video/x-raw ! appsink").format(self.device,
+                                                                                self.screen_table[self.resolution]['w'],
+                                                                                self.screen_table[self.resolution]['h'])
+                    app.logger.info(source)
+                    dataloader = LoadStreams(source)  # Webcam
+                    self.is_init = True
+                except Exception as ex:
+                    self.data =  generate_image_with_text(f"Traceback error: {ex}")
+                    self.is_ok = False                
 
-                # Jancee updated
-                # sleep(0.05)
-                counter = counter + 1
+        # Send RPi hostname with each image
+        # This might be unnecessary in this pub sub mode, as the receiver will
+        #    already need to know our address and can therefore distinguish streams
+        # Keeping it anyway in case you wanna send a meaningful tag or something
+        #    (or have a many to many setup)
+        rpi_name = socket.gethostname()
 
-    except (KeyboardInterrupt, SystemExit):
-        print('Exit due to keyboard interrupt')
-    except Exception as ex:
-        print('Python error with no Exception handler:')
-        print('Traceback error:', ex)
-        traceback.print_exc()
-    finally:
-        sender.close()
-        sys.exit()
+        try:
+            counter = 0
+            for path, im, im0s, vid_cap, s in dataloader:
+                for img in im0s:
+                    # img = cv2.resize(img, (1280,720), interpolation=cv2.INTER_LINEAR)
+                    ret_code, jpg_buffer = cv2.imencode(
+                        ".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), self.jpeg_quality ])
+                    # sender.send_jpg(rpi_name, base64.b64encode(jpg_buffer))
 
-def init_streams():
-    t = threading.Thread(target=start_streams)
-    t.start()
+                    self.data = jpg_buffer
+
+                    # sender.send_jpg(rpi_name, jpg_buffer)
+
+                    #sender.send_jpg(rpi_name, img)
+
+                    # jancee updated
+                    # print("Sent frame {}".format(counter))
+
+                    # Jancee updated
+                    # sleep(0.05)
+                    counter = counter + 1
+
+        except (KeyboardInterrupt, SystemExit):
+            print('Exit due to keyboard interrupt')
+        except Exception as ex:
+            print('Python error with no Exception handler:')
+            print('Traceback error:', ex)
+            traceback.print_exc()
+        finally:
+            sys.exit()
+
+
+
 
 if __name__ == "__main__":
-    init_streams()
+    dataloader = DataLoadderThread()
+    dataloader.start()
+
+    @app.route('/health')
+    def http_get_request_health():
+        return 'ok'
+
+    @app.route('/')
+    def hello():
+        resolution = request.args.get("resolution")
+        device = request.args.get("localAddress")
+        rtsp = request.args.get("rtspUrl")
+        dataloader.set_args(resolution,device,rtsp)
+        if dataloader.is_ok:
+            return base64.b64encode(dataloader.data), 200, [("ok",1)] 
+        else:
+            return base64.b64encode(dataloader.data), 200, [("ok",0)] 
+
+
     app.run(host="0.0.0.0", port=5550)
+
