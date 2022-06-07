4547c382d9bc012d744e526307ce5b10b3df2788
Baozhu Zuo <zuobaozhu@gmail.com>
2022-06-07 10:29:02 +0800

    dataloader: remove time.sleep, more faster

--- python/dataloaders.py
+++ python/dataloaders.py
@@ -259,7 +259,6 @@ class LoadStreams:
                     LOGGER.warning('WARNING: Video stream unresponsive, please check your IP camera connection.')
                     self.imgs[i] = np.zeros_like(self.imgs[i])
                     cap.open(stream)  # re-open stream if signal was lost
-            time.sleep(1 / self.fps[i])  # wait time
         cap.release()  # close camera, only self.killed is True
 
     def __iter__(self):
